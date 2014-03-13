31ae5642ae5c22267cd4499c68de8e66a4296f9a
Remsnet LTD, <remsnet@users.noreply.github.com>
2014-03-14 00:20:17 +0100

    Create wget-nagios-docs-4.0.3.sh

---
+++ wget-nagios-docs-4.0.3.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+VER=4.0.3
+
+mkdir /tmp/nagios-docs-$VER
+cd /tmp/nagios-docs-$VER
+wget --max--max -r -l 2edirect=3  -r -l 2edirect=3 --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/toc.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/about.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/beginners.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/upgrading.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring-windows.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring-linux.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring-netware.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring-printers.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring--max -r -l 2edirect=3  -r -l 2outers.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/monitoring-publicservices.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/config.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/configmain.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/startstop.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/plugins.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/macros.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/macrolist.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/hostchecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/servicechecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/activechecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/passivechecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/passivechecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/statetypes.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/timeperiods.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/networkreachability.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/notifications.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/cgis.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/extcommands.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/eventhandlers.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/volatileservices.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/freshness.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/distributed.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/redundancy.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/flapping.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/escalations.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/oncallrotation.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/clusters.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/dependencies.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/stalking.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/perfdata.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/downtime.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/adaptive.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/dependencychecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/cachedchecks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/passivestatetranslation.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/checkscheduling.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/cgiincludes.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/objectinheritance.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/objecttricks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/security.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/cgisecurity.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/tuning.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/faststartup.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/largeinstalltweaks.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/nagiostats.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/mrtggraphs.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/integration.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/int-snmptrap.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/int-tcpwrappers.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/addons.html
+wget --max -r -l 2edirect=3  -r -l 2 http://nagios.sourceforge.net/docs/nagioscore/4/en/pluginapi.html
